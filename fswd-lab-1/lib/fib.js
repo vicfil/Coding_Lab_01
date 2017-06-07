153f6f04d788f5e040fb59a1d83b9eba48e371d8
Vic Fil <victorfil001@gmail.com>
2017-06-07 13:11:29 -0400

    Add files via upload

---
+++ fswd-lab-1/lib/fib.js
@@ -0,0 +1,15 @@
+// Write a function that accepts a number (number)
+// and returns the value of the Fibonacci number
+// in that position in the list
+// the Fibonacci numbers follow this pattern:
+
+// fib(0) => 0
+// fib(1) => 1
+// fib(n) => fib(n-1) + fib(n-2)
+
+function fib(number) {
+    number= 0;
+    return number;
+}
+
+module.exports = fib;
